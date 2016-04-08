5317ce568c39b9caeeec17041ca4b41a4b5a2ea7
zhangjm <zhangjm@gmail.com>
2016-04-08 15:19:53 +0800

    centos k8s deployment

---
+++ env.sh
@@ -0,0 +1,6 @@
+export K8S_VERSION="1.2.2"
+export MASTER="root@192.168.1.10"
+export NODES="root@192.168.1.11 root@192.168.1.12 root@192.168.1.13 root@192.168.1.14 root@192.168.1.15 root@192.168.1.16"
+export NUM_NODES=6
+export SERVICE_CLUSTER_IP_RANGE="10.254.0.0/16"
+export KUBERNETES_PROVIDER=centos
