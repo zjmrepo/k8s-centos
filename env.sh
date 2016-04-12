e38f36ea9ad08325db9a05e021358bf7ddb76f4b
zhangjm <zhangjm@gmail.com>
2016-04-12 10:29:07 +0800

    add package version in env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,7 @@
 export K8S_VERSION="1.2.2"
+export FLANNEL_VERSION="0.5.5"
+export ETCD_VERSION="2.2.1"
+
 export MASTER="root@192.168.1.10"
 export NODES="root@192.168.1.11 root@192.168.1.12 root@192.168.1.13 root@192.168.1.14 root@192.168.1.15 root@192.168.1.16"
 export NUM_NODES=6
